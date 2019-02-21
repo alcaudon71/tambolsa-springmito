85cef7f8dd56936d14a0d2822e25374eae032bc8
Jose Tamborero <jose.sanchez.tamborero@itbo-ericsson.com>
2019-02-21 10:07:37 +0100

    Original

---
+++ springMito/src/main/java/com/mitocode/beans/Mundo.java
@@ -0,0 +1,19 @@
+package com.mitocode.beans;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class Mundo {
+
+	@Value("Hola Mundo!")
+	private String saludo;
+	
+	public String getSaludo() {
+		return saludo;
+	}
+	
+	// Inyeccion de dependencias
+	public void setSaludo(String saludo) {
+		this.saludo = saludo;
+	}
+	
+}
